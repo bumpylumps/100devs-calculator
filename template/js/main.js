391d52748295a677f9f615177cd59d353a3edebc
bumpyLumps <alexander.fulop.art@gmail.com>
2022-04-06 14:20:23 -0400

    Finished styling of calculator and created TODO for js functionality

--- template/js/main.js
+++ template/js/main.js
@@ -1,3 +1,13 @@
 //Follow best practices and use a constructor
 
-
+function Calculator(){
+    /*functions needed
+    1) Print - must print the buttons that are pressed in window
+    2) Sum - if + is in window and = is pressed, perform addition
+    3) Difference - if - is in window and = is pressed, perform subtraction
+    4) Product - if * is in window and = is pressed, perform multilplication
+    5) Quotient - if / is in window and = is pressed, perform division
+    6) Fortune - if fortune is pressed, return random fortune 
+    7) Try and set up for multiple math functions in window (i.e. x + y / 4)
+    */
+}
