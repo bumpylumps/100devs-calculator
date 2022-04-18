f1d4f36658babe3e16562636cf0fce90b8ac8114
bumpyLumps <alexander.fulop.art@gmail.com>
2022-04-18 16:35:46 -0400

    added fortune functionality

--- template/js/main.js
+++ template/js/main.js
@@ -1,6 +1,11 @@
 //Follow best practices and use a constructor
 
-function Calculator(){
+
+
+class Calculator {
+    constructor(){
+
+    }
     /*functions needed
     1) Print - must print the buttons that are pressed in window
     2) Sum - if + is in window and = is pressed, perform addition
@@ -10,4 +15,32 @@ function Calculator(){
     6) Fortune - if fortune is pressed, return random fortune 
     7) Try and set up for multiple math functions in window (i.e. x + y / 4)
     */
+
+    //fortune
+    fortune(){
+    let resultNum = Math.floor(Math.random()*6)
+    if(resultNum === 0){
+        document.querySelector('h1').innerText = 'Seek medical attention'
+    } else if(resultNum === 1){
+        document.querySelector('h1').innerText = 'Goblins Incoming'
+    } else if(resultNum === 2){
+        document.querySelector('h1').innerText = 'Good financial fortune inbound'
+    } else if(resultNum === 3){
+        document.querySelector('h1').innerText = 'Follow your nose'
+    } else if(resultNum === 4){
+        document.querySelector('h1').innerText = 'Write down the thing'
+    } else if(resultNum === 5){
+        document.querySelector('h1').innerText = 'I\'m tired: ask the Tarot'
+    }
+
+    console.log(resultNum)
+    
 }
+
+}
+
+
+let calc = new Calculator()
+
+document.getElementById('fortune').addEventListener('click', calc.fortune)
+
