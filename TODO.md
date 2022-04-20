62733db3f8db132bf299e93a4b1506d1d2b90252
bumpyLumps <alexander.fulop.art@gmail.com>
2022-04-19 21:07:20 -0400

    got division working, got numbers/operators to print after button press

---
+++ TODO.md
@@ -0,0 +1,4 @@
+
+- Add functionality to every arithmetic operator
+- Clean up results (to 2 decimal spaces)
+- add more fortunes
--- template/js/main.js
+++ template/js/main.js
@@ -16,6 +16,93 @@ class Calculator {
     7) Try and set up for multiple math functions in window (i.e. x + y / 4)
     */
 
+    //print button results
+    printSeven(){
+        document.querySelector('h1').innerText += 7
+    }
+
+    printEight(){
+        document.querySelector('h1').innerText += 8
+    }
+
+    printNine(){
+        document.querySelector('h1').innerText += 9
+    }
+
+    printFour(){
+        document.querySelector('h1').innerText += 4
+    }
+
+    printFive(){
+        document.querySelector('h1').innerText += 5
+    }
+
+    printSix(){
+        document.querySelector('h1').innerText += 6
+    }
+
+    printOne(){
+        document.querySelector('h1').innerText += 1
+    }
+
+    printTwo(){
+        document.querySelector('h1').innerText += 2
+    }
+
+    printThree(){
+        document.querySelector('h1').innerText += 3
+    }
+
+    printZero(){
+        document.querySelector('h1').innerText += 0
+    }
+
+    printDivide(){
+        document.querySelector('h1').innerText += '/'
+    }
+
+    printMultiply(){
+        document.querySelector('h1').innerText += '*'
+    }
+
+    printAdd(){
+        document.querySelector('h1').innerText += '+'
+    }
+
+    printSubtract(){
+        document.querySelector('h1').innerText += '-'
+    }
+
+
+
+    //get result
+    evaluateResult(){
+        let resultString = document.querySelector('h1').innerText  
+        let position = resultString.indexOf('/')
+        let numerator = resultString.slice(0, position)
+           console.log(numerator)
+        let denominator = resultString.slice(position +1, resultString.length)
+           console.log(denominator)
+        if(resultString.includes('/')){ 
+           resultString = numerator / denominator
+           document.querySelector('h1').innerText = resultString
+        } 
+    }
+
+    
+
+
+
+
+
+
+
+    //clear
+    clear(){
+        document.querySelector('h1').innerText = ''
+    }
+
+
     //fortune
     fortune(){
     let resultNum = Math.floor(Math.random()*6)
@@ -43,4 +130,20 @@ class Calculator {
 let calc = new Calculator()
 
 document.getElementById('fortune').addEventListener('click', calc.fortune)
+document.getElementById('seven').addEventListener('click', calc.printSeven)
+document.getElementById('eight').addEventListener('click', calc.printEight)
+document.getElementById('nine').addEventListener('click', calc.printNine)
+document.getElementById('divide').addEventListener('click', calc.printDivide)
+document.getElementById('four').addEventListener('click', calc.printFour)
+document.getElementById('five').addEventListener('click', calc.printFive)
+document.getElementById('six').addEventListener('click', calc.printSix)
+document.getElementById('multiply').addEventListener('click', calc.printMultiply)
+document.getElementById('one').addEventListener('click', calc.printOne)
+document.getElementById('two').addEventListener('click', calc.printTwo)
+document.getElementById('three').addEventListener('click', calc.printThree)
+document.getElementById('add').addEventListener('click', calc.printAdd)
+document.getElementById('zero').addEventListener('click', calc.printZero)
+document.getElementById('clear').addEventListener('click', calc.clear)
+document.getElementById('equals').addEventListener('click', calc.evaluateResult)
+document.getElementById('subtract').addEventListener('click', calc.printSubtract)
 
